13e25aba054d09bb76c7355c35db1e81fc015268
Dani Adkins <danirenaeadkins12@gmail.com>
2016-10-30 17:39:42 -0500

    added styling and functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,86 +1,57 @@
 "use strict";
 
 $(document).ready(function(){
-  $('#click-me').on('click', ()=>{
+  let chatItem = $('input').val();
+  let output = $('.todo');
+  let currentSelection = "";
+
+  $('#click-me').on('click', (e)=>{
     $('input').html("");
     let chatItem = $('input').val();
-    console.log("heyyyy", chatItem);
     let output = $('.todo');
     // dom.html(''); // this will replace what is there in dom
-    output.append(`<span class='out'>${chatItem}</span> <button class='edit'>Edit</button>`); //this prints out the chat item to the dom
-
+    output.append(`<li><span class='chat'><input type='checkbox'>${chatItem}</span><button class='edit'>Edit</button></li>`); //this prints out the chat item to the dom
+    chatItem.this.val = ""; // how can I clear the input?
   });
 
-$(document).on('click', 'span', function(){
+$(document).on('click', '.chat', function(){
   // $(this).fadeOut('slow');
 });
 
+
 $(document).on('click', '.edit', function(event){
-  $(event.target).text();
+let chatItem = $('input').val();
+  currentSelection = event.currentTarget;
+  currentSelection.classList.add("edit");
+
+  // $(event.currentTarget).text();
+  // chatItem.focus();
 console.log(event);
 });
 
 
+
 });
 
+// jQuery(function($) {
 
-// let removeItem = $(event.target)
+//   // Save the initial values of the inputs as placeholder text
+//   $('#theForm input').attr("data-placeholdertext", function() {
+//     return this.value;
+//   });
 
+//   // Hook up a handler to delete the placeholder text on focus,
+//   // and put it back on blur
+//   $('#theForm')
+//     .delegate('input', 'focus', function() {
+//       if (this.value === $(this).attr("data-placeholdertext")) {
+//         this.value = '';
+//       }
+//     })
+//     .delegate('input', 'blur', function() {
+//       if (this.value.length == 0) {
+//         this.value = $(this).attr("data-placeholdertext");
+//       }
+//     });
 
-// $('output').click(function() {
-//   $('output').remove();
-// });
-
-
-// function handler( event ) {
-//   var target = $( event.target );
-//   if ( target.is( "li" ) ) {
-//     target.children().toggle();
-//   }
-// }
-// $( "ul" ).click( handler ).find( "ul" ).hide();
-
-
-
-    // $('click-me').button('reset');
-    // console.log("this is resetting")
-
-// var userInput = document.getElementById("user-input");
-// var editInput = document.getElementById("edit-input");
-// var messageContainer = document.getElementById("entered-messages");
-// var clearMessages = document.getElementById("clear-messages");
-// var messages = document.getElementsByClassName("messages");
-// var userDiv = document.getElementById("choose-user").children;
-// var current_time = new moment().format("LT");
-// var user = "";
-
-// document.querySelector("body").addEventListener("click", function(e) {
-//   if (e.target.id === "delete-button") {
-//     var elementToDelete = e.target.parentNode;
-//     Chatty.removeFromDOM(elementToDelete);
-//     var idToDelete = e.target.parentNode.id;
-
-//     Chatty.removeFromArray(idToDelete);
-//   }
-//   if (e.target.id === "clear-messages") {
-//     var allDivs = messageContainer.children;
-//     if (allDivs.length != 0){
-//       Chatty.removeAllFromArray(allDivs);
-//       messageContainer.innerHTML = "";
-//       clearMessages.setAttribute("disabled", true);
-//     }
-//   }
-//   if (e.target.id === "edit-button") {
-//     var targetMessage = e.target;
-//     var elementToEdit = e.target.parentNode;
-//     var timeEdit = elementToEdit.children[1];
-//     var userEdit = elementToEdit.children[2];
-//     console.log(userEdit);
-//     editButton(targetMessage, timeEdit, userEdit);
-//     userInput.classList.add("hidden");
-//     editInput.classList.remove("hidden");
-//   }
-//   if (e.target.type === "radio") {
-//     userInput.focus();
-//   }
 // });
