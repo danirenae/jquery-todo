52022f21df2cabdc61bc9617bb56a62c2d274f61
Dani Adkins <danirenaeadkins12@gmail.com>
2016-10-31 21:31:33 -0500

    firebase practice

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,37 +1,132 @@
 "use strict";
 
-$(document).ready(function(){
-  let chatItem = $('input').val();
-  let output = $('.todo');
-  let currentSelection = "";
+  let apiKeys = {};
 
-  $('#click-me').on('click', (e)=>{
-    $('input').html("");
-    let chatItem = $('input').val();
-    let output = $('.todo');
-    // dom.html(''); // this will replace what is there in dom
-    output.append(`<li><span class='chat'><input type='checkbox'>${chatItem}</span><button class='edit'>Edit</button></li>`); //this prints out the chat item to the dom
-    chatItem.this.val = ""; // how can I clear the input?
+  function putTodoInDOM (){
+    FbAPI.getTodos(apiKeys).then(function(items){
+    console.log("items from FB", items);
+    $('#completed-tasks').html("");
+    $('#incomplete-tasks').html("");
+    items.forEach(function(item){
+      if(item.isCompleted === true){
+      let newListItem = '<li>';
+      newListItem+='<div class="col-xs-8">';
+      newListItem+='<input class="checkboxStyle" type="checkbox" checked>';
+      newListItem+=`<label class="inputLabel">${item.task}</label>`;
+      newListItem+='<input type="text" class="inputTask">';
+      newListItem+='</div>';
+      newListItem+='</li>';
+      //apend to list
+      $('#completed-tasks').append(newListItem);
+      } else {
+         let newListItem = '<li>';
+      newListItem+='<div class="col-xs-8">';
+      newListItem+='<input class="checkboxStyle" type="checkbox">';
+      newListItem+=`<label class="inputLabel">${item.task}</label>`;
+      newListItem+='<input type="text" class="inputTask">';
+      newListItem+='</div>';
+      newListItem+='<div class="col-xs-4">';
+      newListItem+='<button class="btn btn-default col-xs-6 edit">Edit</button>';
+      newListItem+=`<button class="btn btn-danger col-xs-6 delete" data-fbid='${item.id}'>Delete</button>`;
+      newListItem+='</div>';
+      newListItem+='</li>';
+      //apend to list
+      $('#incomplete-tasks').append(newListItem);
+      }
+    });
+  });
+}
+
+$(document).ready(function(){
+  FbAPI.firebaseCredentials().then(function(keys){
+    console.log("keys", keys);
+    apiKeys = keys;
+      firebase.initializeApp(apiKeys);
+      putTodoInDOM();
+      });
+
+$('#click-me').on('click', function(){
+let newItem = {
+  "task": $('#input').val(), //this grabs the value of the input and puts it into object that we are creating
+  "isCompleted": false
+};
+  FbAPI.addTodo(apiKeys, newItem).then(function(){
+    putTodoInDOM();
+  });
+});
+
+$('ul').on("click", ".delete", function(){
+  let itemId = $(this).data("fbid");
+  FbAPI.deleteTodo(apiKeys, itemId).then(function(){
+  putTodoInDOM();
+  });
+});
+
   });
 
-$(document).on('click', '.chat', function(){
-  // $(this).fadeOut('slow');
-});
-
-
-$(document).on('click', '.edit', function(event){
-let chatItem = $('input').val();
-  currentSelection = event.currentTarget;
-  currentSelection.classList.add("edit");
-
-  // $(event.currentTarget).text();
-  // chatItem.focus();
-console.log(event);
-});
 
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//   let chatItem = $('input').val();
+//   let output = $('.todo');
+//   let currentSelection = "";
+
+
+
+//   $('#click-me').on('click', (e)=>{
+//     $('input').html("");
+//     let chatItem = $('input').val();
+//     let output = $('.todo');
+//     // dom.html(''); // this will replace what is there in dom
+//     output.append(`<li><span class='chat'><input type='checkbox'>${chatItem}</span><button class='edit'>Edit</button></li>`); //this prints out the chat item to the dom
+//     chatItem.this.val = ""; // how can I clear the input?
+//   });
+
+// $(document).on('click', '.chat', function(){
+//   // $(this).fadeOut('slow');
+// });
+
+
+// $(document).on('click', '.edit', function(event){
+// let chatItem = $('input').val();
+//   currentSelection = event.currentTarget;
+//   currentSelection.classList.add("edit");
+
+//   // $(event.currentTarget).text();
+//   // chatItem.focus();
+// console.log(event);
+// });
+
+
+
+// });
 
 // jQuery(function($) {
 
